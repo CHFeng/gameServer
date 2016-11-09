7f0105986316e8df22c9f5a0538a498d94e13fe4
danielfeng <copam7171@gmail.com>
2016-11-09 10:23:20 +0800

    1.修改client測試程式

--- client.js
+++ client.js
@@ -1,14 +1,17 @@
 var net = require('net');
 
 var HOST = '127.0.0.1';
-var PORT = 1688;
+var PORT = 16889;
 
 var client = new net.Socket();
 
 client.connect(PORT, HOST, function() {
+    var writeData = new Buffer(3);
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
-    client.write('I am Chuck Norris!', function(){
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+    writeData.writeUInt8(1, 0);
+    writeData.writeUInt16LE(0, 1); 
+    client.write(writeData, function(){
         console.log("write data to socket finish");
     });
 
@@ -21,8 +24,12 @@ client.on('error', function(err) {
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
 client.on('data', function(data) {
-    
-    console.log('DATA from Server: ' + data);
+    console.log("Data length:%d", data.length);
+    for (var i = 0; i < 100; i++) {
+        if (data[i] == 1) {
+            console.log("Client:%d online", i+1);
+        }
+    }
     // Close the client socket completely
     client.destroy();
     
